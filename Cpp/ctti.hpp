ead6fc7959c077f26114d0e00ca285e5d8556c54
QuarticCat <QuarticCat@protonmail.com>
2022-04-26 19:43:38 -0700

    refactor ctti.hpp

--- Cpp/ctti.hpp
+++ Cpp/ctti.hpp
@@ -2,10 +2,15 @@
 
 #include <string_view>
 
-template<typename T>
-constexpr std::string_view pretty_function() {
-    return __PRETTY_FUNCTION__;
+namespace detail {
+
+constexpr uint64_t fnv1a_hash(std::string_view str) {
+    uint64_t hash = 14695981039346656037ull;
+    for (char c: str) hash = (hash ^ c) * 1099511628211ull;
+    return hash;
 }
+
+}  // namespace detail
 
 /// Return the name of the given type in compile-time.
 ///
@@ -24,31 +29,35 @@ constexpr std::string_view pretty_function() {
 template<typename T>
 constexpr std::string_view type_name() {
 #if defined(__clang__)
-    constexpr const char PREFIX[] = "std::string_view pretty_function() [T = ";
-    constexpr const char SUFFIX[] = "]";
+    const char prefix[] = "std::string_view type_name() [T = ";
+    const char suffix[] = "]";
 #elif defined(__GNUC__)
-    constexpr const char PREFIX[] = "constexpr std::string_view pretty_function() [with T = ";
-    constexpr const char SUFFIX[] = "; std::string_view = std::basic_string_view<char>]";
+    const char prefix[] = "constexpr std::string_view type_name() [with T = ";
+    const char suffix[] = "; std::string_view = std::basic_string_view<char>]";
 #endif
 
-    auto name = pretty_function<T>();
-    name.remove_prefix(sizeof(PREFIX) - 1);
-    name.remove_suffix(sizeof(SUFFIX) - 1);
+    std::string_view name = __PRETTY_FUNCTION__;
+    name.remove_prefix(sizeof(prefix) - 1);
+    name.remove_suffix(sizeof(suffix) - 1);
     return name;
 }
 
-constexpr size_t fnv1a_hash(std::string_view str) {
-    constexpr size_t FNV_BASIS = 14695981039346656037ull;
-    constexpr size_t FNV_PRIME = 1099511628211ull;
-
-    size_t hash = FNV_BASIS;
-    for (char c: str) hash = (hash ^ c) * FNV_PRIME;
-    return hash;
-}
-
 /// Return the hash of the given type in compile-time.
+///
+/// Ref: https://blog.quarticcat.com/posts/cpp-ctti/
+///
+/// # Examples
+///
+/// ```
+/// assert(type_hash<int>() != type_hash<char>());
+/// ```
+///
+/// # Compatibility
+///
+/// - GCC / Clang
+/// - C++17 or higher
 template<typename T>
-constexpr size_t type_hash() {
+constexpr uint64_t type_hash() {
     auto name = type_name<T>();
-    return fnv1a_hash(name);
+    return detail::fnv1a_hash(name);
 }
