e3e2f94e3ce15bce1c945c234d1e87ccc07af0b8
QuarticCat <QuarticCat@protonmail.com>
2022-04-26 22:12:53 -0700

    add poor_mans_concept.cpp

---
+++ Cpp/poor_mans_concept.cpp
@@ -0,0 +1,17 @@
+//! An example of how to simulate concept before C++20. It imitates the code below:
+//!
+//! ```
+//! template<class T>
+//! concept Arithmetic = std::is_arithmetic_v<T>;
+//!
+//! template<Arithmetic T>
+//! void foo(T);
+//! ```
+
+#include <type_traits>
+
+template<class T, class = std::enable_if_t<std::is_arithmetic_v<T>>>
+using Arithmetic = T;
+
+template<class T>
+void foo(Arithmetic<T>);
